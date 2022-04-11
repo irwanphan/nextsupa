366b3b84167bbdcfc2fe221edb1042ac8f504a3b
@irwanphan <irwanphan@gmail.com>
2022-04-11 22:38:57 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+// Import the generated Prisma client
+import { PrismaClient } from '@prisma/client'
+// Instantiate it
+const prisma = new PrismaClient()
 
-import homes from 'data.json';
+// import homes from 'data.json';
 
-export default function Home() {
+export async function getServerSideProps() {
+  // Get all homes
+  const homes = await prisma.home.findMany();
+  // Pass the data to the Home page
+  return {
+    props: {
+      homes: JSON.parse(JSON.stringify(homes)),
+    }
+  };
+}
+
+export default function Home({ homes = [] }) {
   return (
     <Layout>
       <h1 className="text-xl font-medium text-gray-800">
