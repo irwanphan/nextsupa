45c2a92d8845d3f5826f612091620ba862166187
@irwanphan <irwanphan@gmail.com>
2022-04-14 00:28:24 +0700

    upload API export config

--- pages/api/image-upload.js
+++ pages/api/image-upload.js
@@ -9,6 +9,15 @@ const supabase = createClient(
     process.env.SUPABASE_KEY
 )
 
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '10mb',
+        },
+    },
+};
+  
+
 export default async function handler(req, res) {
     // Upload image to Supabase
     if (req.method === 'POST') {
